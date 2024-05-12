a3e629117c7d196b8f18ae400e3dcc2c6a78e6d2
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-05-13 03:09:55 +0600

    Added registration and validation functionality

---
+++ app/api/auth/register/route.js
@@ -0,0 +1,28 @@
+import { userModel } from "@/models/user-model";
+import { dbConnect } from "@/service/mongo";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export const POST = async (request) => {
+  const { fname, lname, email, password } = await request.json();
+
+  await dbConnect();
+
+  const hashPassword = await bcrypt.hash(password, 5);
+
+  const newUser = {
+    name: `${fname} ${lname}`,
+    email,
+    password: hashPassword,
+  };
+
+  try {
+    await userModel.create(newUser); 
+     
+    return new NextResponse("User has been created", { status: 201 });
+  } catch (err) {
+    return new NextResponse(err.message, {
+      status: 500,
+    });
+  }
+};
