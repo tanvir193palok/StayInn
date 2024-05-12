fde5b0610acf6e419c183a2cf17f5b97ae5eb714
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-05-12 19:36:32 +0600

    Sigining with google functionality added

--- components/auth/SocialLogins.jsx
+++ components/auth/SocialLogins.jsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Image from "next/image";
+import { signIn } from "next-auth/react";
 
 const SocialLogins = () => {
+  const handleAuth = (e) => {
+    signIn("google", { callbackUrl: "http://localhost:3000/bookings" });
+  };
   return (
     <>
       <div className="text-center text-xs text-gray-500">or Signup with</div>
@@ -9,7 +15,10 @@ const SocialLogins = () => {
           <Image src="/fb.png" alt="facebook" width={40} height={40} />
           <span>Facebook</span>
         </button>
-        <button className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center">
+        <button
+          onClick={handleAuth}
+          className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center"
+        >
           <Image src="/google.png" alt="google" width={40} height={40} />
           <span>Google</span>
         </button>
