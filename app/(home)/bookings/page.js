d3b759a2ddc53cfdae774a7ffc81a460ca70daf8
Tanvir Ahmed Palok <118113504+tanvir193palok@users.noreply.github.com>
2024-05-13 05:07:47 +0600

    Added provate route

--- app/(home)/bookings/page.js
+++ app/(home)/bookings/page.js
@@ -1,8 +1,15 @@
 import ProfileInfo from "@/components/user/ProfileInfo";
 import PastBooking from "@/components/user/booking/PastBooking";
 import UpcomingBooking from "@/components/user/booking/UpcomingBooking";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-const BookingsPage = () => {
+const BookingsPage = async () => {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <>
       <section className="mt-[100px]">
